388c51dfe49b25327630747566875b12350d0170
jlp0328 <jlp0328@gmail.com>
2017-03-12 14:28:14 -0400

    Resetting form

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 
 var friendsArray = [
   {name: "Katie",
-   link: "",
+   link: "http://www.celebritycarsblog.com/wp-content/uploads/katie-holmes.jpeg",
    responses: [2,4,4,2,1,2,4,3,5,1]},
 
   {name: "Shannon",
-   link: "",
+   link: "http://crasstalk.com/wp-content/uploads/2011/03/Brenda-White-Shirt-Tie-Full.jpg",
    responses: [1,3,4,5,5,2,4,1,5,1]},
 
-  {name: "Julie",
-   link: "",
+  {name: "Jenna",
+   link: "http://cliqueimg.com/cache/posts/178227/jenna-dewan-tatum-just-chopped-her-hair-even-shorter-1576907-1448909883.640x0c.jpg",
    responses: [2,3,4,5,1,2,4,2,3,1]},
 
   {name: "Julie",
-   link: "",
+   link: "https://upload.wikimedia.org/wikipedia/commons/8/89/Julie_Andrews_Park_Hyatt,_Sydney,_Australia_2013.jpg",
    responses: [2,3,4,5,1,2,4,2,3,1]}
 ];
 
